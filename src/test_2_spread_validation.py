b2ae393ef398f31482cd70b1563432cab799c1b4
Ivan Legorreta <ilegorreta@hotmail.com>
2022-10-06 18:46:19 -0500

    Added spread validation

--- src/test_2_spread_validation.py
+++ src/test_2_spread_validation.py
@@ -4,22 +4,24 @@
 import csv
 from datetime import datetime
 
-book_1 = "btc_mxn"
-book_2 = "usd_mxn"
+from utils.SpreadValidator import SpreadValidator
+
+BOOK_1 = "btc_mxn"
+BOOK_2 = "usd_mxn"
 
 
 def main():
+    total_times_above_threshold = 0
     utc_time = datetime.utcnow()
-    year = utc_time.strftime("%Y")
-    month = utc_time.strftime("%m")
-    day = utc_time.strftime("%d")
-    hour = utc_time.strftime("%H")
-    path_1 = f"/workspaces/bitso-test/data/bitso/{book_1}/{year}/{month}/{day}/{hour}/files.csv"
-    path_2 = f"/workspaces/bitso-test/data/bitso/{book_1}/{year}/{month}/{day}/{hour}/files.csv"
-
-    with open(path_1, "r") as data:
-        for line in csv.DictReader(data):
-            print(line)
+    book_1_validator = SpreadValidator(BOOK_1, utc_time)
+    book_2_validator = SpreadValidator(BOOK_2, utc_time)
+    if book_1_validator.is_above and book_2_validator.is_above:
+        total_times_above_threshold = 2
+    elif book_1_validator.is_above or book_2_validator.is_above:
+        total_times_above_threshold = 1
+    else:
+        total_times_above_threshold = 0
+    print(f"The total of order book above threshold was: {total_times_above_threshold}")
 
 
 if __name__ == "__main__":
---
+++ src/utils/SpreadValidator.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import csv
+
+
+class SpreadValidator:
+    """Class template for validating bid-ask spread for a giveen order book
+
+    Constructor Args:
+        book: String corresponding to the given order book
+        utc_time: Datetime object indicating the current UTC time
+    """
+
+    def __init__(self, book, utc_time):
+        self.book = book
+        self.utc_time = utc_time
+        self.path = None
+        self.is_above = None
+
+    def validate_spread(self):
+        """Validate if the bid-ask spread is above the given threshold"""
+        year = self.utc_time.strftime("%Y")
+        month = self.utc_time.strftime("%m")
+        day = self.utc_time.strftime("%d")
+        hour = self.utc_time.strftime("%H")
+        self.path = f"/workspaces/bitso-test/data/bitso/{self.book}/{year}-{month}-{day}/{hour}/files.csv"
+        with open(self.path, "r") as data:
+            for line in csv.DictReader(data):
+                if line["is_above"] == "True":
+                    self.is_above = True
+                else:
+                    self.is_above = False
